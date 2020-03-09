13033ce5af9442fa35dbf501947ccf647f7b5b4a
Alexander Valdez <Cheezfri@users.noreply.github.com>
2020-03-08 23:03:06 -0400

    Added base templates

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Alex',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 8th, 2020'
+    },
+    {
+        'author': 'Alec',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 9th, 2020'
+    }
+]
 
 
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+def home(request):  # Views either return a http response or exception
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)  # http response is returned in background
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
