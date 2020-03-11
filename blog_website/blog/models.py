8ef9ca450fda63a021bf04e86fb78b62777333bf
Alexander Valdez <Cheezfri@users.noreply.github.com>
2020-03-11 02:25:26 -0400

    Adding a database

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=100)  # textfield Unrestricted text
+    date_posted = models.DateTimeField(default=timezone.now)  # No parenthesis so function isn't executed but passed in
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # If user is deleted, all posts are deleted with CAS
